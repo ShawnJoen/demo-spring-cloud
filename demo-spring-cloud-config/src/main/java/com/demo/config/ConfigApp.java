e8aa9e7e3f85c9e49ded8b14988e70f2f6ec8f30
quanchunlindellnote <quanchunlin108@163.com>
2019-03-13 13:39:25 +0800

    add spring-cloud-config-server

--- demo-spring-cloud-config/src/main/java/com/demo/config/ConfigApp.java
+++ demo-spring-cloud-config/src/main/java/com/demo/config/ConfigApp.java
@@ -1,14 +1,16 @@
-package com.demo.user;
+package com.demo.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableDiscoveryClient
-public class UserApp {
+public class ConfigApp {
     public static void main(String[] args) {
-        SpringApplication.run(UserApp.class, args);
+        SpringApplication.run(ConfigApp.class, args);
     }
 
 }
--- demo-spring-cloud-user-service/src/main/java/com/demo/user/controller/UserInfoController.java
+++ demo-spring-cloud-user-service/src/main/java/com/demo/user/controller/UserInfoController.java
@@ -1,13 +1,22 @@
 package com.demo.user.controller;
 
 import com.demo.common.entity.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@RefreshScope
 public class UserInfoController {
+    @Value("${info.foo}")
+    String name;
+    @Value("${spring.aaaa.eeeee11}")
+    String name2;
+    @Value("${spring.aaaa.eeeee22}")
+    String name3;
 
     @GetMapping("/user/{id}")
     public User getUser(@PathVariable("id") Integer id, HttpServletRequest request){
@@ -15,7 +24,7 @@ public class UserInfoController {
         User uesr = new User();
         uesr.setId(id);
         uesr.setAge(30);
-        uesr.setName("哈哈" + id);
+        uesr.setName("哈哈" + id + name + ":" + name2 + ":" + name3);
         return uesr;
     }
 
