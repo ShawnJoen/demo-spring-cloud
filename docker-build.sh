12ed8a938afaabf4362829a63fd8ada6d595a381
quanchunlindellnote <quanchunlin108@163.com>
2019-03-25 14:28:45 +0800

    docker build

--- docker-build.sh
+++ docker-build.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-cd /www/demo-spring-cloud/demo-spring-cloud-registry
+echo "===> registry"
+cd /www/demo-spring-cloud/demo-spring-cloud-registry/
 docker build -t demo/demo-spring-cloud-registry:1.0.0 .
 
-cd /www/demo-spring-cloud/demo-spring-cloud-config
+echo "===> config"
+cd /www/demo-spring-cloud/demo-spring-cloud-config/
 docker build -t demo/demo-spring-cloud-config:1.0.0 .
 
-cd /www/demo-spring-cloud/demo-spring-cloud-user-service
+echo "===> user-service"
+cd /www/demo-spring-cloud/demo-spring-cloud-user-service/
 docker build -t demo/demo-spring-cloud-user-service:1.0.0 .
 
-cd /www/demo-spring-cloud/demo-spring-cloud-gateway
+echo "===> user2-service"
+cd /www/demo-spring-cloud/demo-spring-cloud-user2-service/
+docker build -t demo/demo-spring-cloud-user2-service:1.0.0 .
+
+echo "===> order-service"
+cd /www/demo-spring-cloud/demo-spring-cloud-order-service/
+docker build -t demo/demo-spring-cloud-order-service:1.0.0 .
+
+echo "===> gateway"
+cd /www/demo-spring-cloud/demo-spring-cloud-gateway/
 docker build -t demo/demo-spring-cloud-gateway:1.0.0 .
